d8284353d58ac3ef687b5b03576ac898da26487b
RJDG <81928911+palomajnm@users.noreply.github.com>
2026-05-11 11:38:51 +0200

    p110526

---
+++ script.js
@@ -0,0 +1,24 @@
+//poner el modo oscuro o claro dependiendo de la preferencia del usuario guardada en localStorage
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'light') {
+    body.classList.add('light-mode');
+    themeToggle.textContent = '☀️'; // Icono de sol para modo claro
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    
+    let theme = 'dark';
+    if (body.classList.contains('light-mode')) {
+        theme = 'light';
+        themeToggle.textContent = '☀️';
+    } else {
+        themeToggle.textContent = '🌙';
+    }
+    
+    localStorage.setItem('theme', theme);
+});
